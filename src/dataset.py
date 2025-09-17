cc19aa512378cbf75e3855ac3b0b005548a201cb
GonzaMarolda <58080986+GonzaMarolda@users.noreply.github.com>
2025-09-17 13:22:37 -0300

    Preprocessing & Dataset

---
+++ src/dataset.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+
+CSV_PATH = "../data/processed.csv"
+
+def get_dataloaders(batch_size=32, test_size=0.2):
+    df = pd.read_csv(CSV_PATH)
+
+    X = df.drop(columns=["pct_pos_total_norm"]).values.astype("float32") # Pytorch works better with float32
+    y = df["pct_pos_total_norm"].values.astype("float32").reshape(-1, 1) # Reshape for as many rows as needed and 1 column
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+    train_dataset = TensorDataset(torch.tensor(X_train), torch.tensor(y_train))
+    test_dataset = TensorDataset(torch.tensor(X_test), torch.tensor(y_test))
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
---
+++ src/preprocessing.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import ast
+import warnings
+warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
+
+RAW_PATH = "../data/raw.csv"
+PROCESSED_PATH = "../data/processed.csv"
+EXPLORATION_PATH = "../data/exploration.csv"
+
+raw_df = pd.read_csv(RAW_PATH)
+print(f"[INFO] Games in raw data: {len(raw_df)}")
+
+# Filter by date (2015–2025) 
+raw_df["release_date"] = pd.to_datetime(raw_df["release_date"], errors="coerce")
+raw_df = raw_df.dropna(subset=["release_date"])
+raw_df = raw_df[(raw_df["release_date"].dt.year >= 2015) & (raw_df["release_date"].dt.year <= 2025)]
+print(f"[INFO] Games after filtering by date (2015–2025): {len(raw_df)}")
+
+# Filter out lesser known games (less than 100000 owners)
+filtered_out = ["0 - 0", "0 - 20000", "20000 - 50000", "50000 - 100000"]
+raw_df = raw_df[raw_df["estimated_owners"].isin(filtered_out) == False]
+print(f"[INFO] Games after filtering by estimated owners (x >= 100,000): {len(raw_df)}")
+
+# Normalize year [2015–2030] to [0–1]
+raw_df["year_norm"] = (raw_df["release_date"].dt.year - 2015) / (2030 - 2015)
+# Normalize price [0–100] to [0–1]
+raw_df["price_norm"] = raw_df["price"].clip(0, 80) / 80
+# Normalize required_age [0–21] to [0–1]
+raw_df["required_age_norm"] = raw_df["required_age"].clip(0, 21) / 21
+# Normalize pct_pos_total [0–100] to [0–1]
+raw_df["pct_pos_total_norm"] = raw_df["pct_pos_total"].clip(0, 100) / 100
+
+# Top 150 tags and one-hot encoding
+top_n_tags = 150
+all_tags = raw_df["tags"].apply(lambda tags: ast.literal_eval(tags)).explode()
+top_tags = all_tags.value_counts().head(top_n_tags).index
+print(f"[INFO] Total tags found: {all_tags.nunique()}")
+print(f"[INFO] Using top {top_n_tags} most common")
+
+for tag in top_tags:
+    col_name = f"tag_{tag}".replace(" ", "_").replace("-", "_")
+    raw_df[col_name] = raw_df["tags"].apply(lambda game_tags: 1 if tag in game_tags else 0)
+
+# Count supported languages and normalize [0–15] to [0–1]
+raw_df["lang_count"] = raw_df["supported_languages"].apply(lambda langs: len(ast.literal_eval(langs)))
+raw_df["lang_norm"] = raw_df["lang_count"].clip(0, 15) / 15
+
+# Add supports_english and is_for_adults column
+raw_df["supports_english"] = raw_df["supported_languages"].apply(
+    lambda langs: "English" in ast.literal_eval(langs)
+)
+raw_df["for_mature_audiences"] = raw_df["required_age"].apply(lambda age: int(age) >= 17)
+
+# Build processed dataframe
+output_cols = [
+    "price_norm", 
+    "required_age_norm", 
+    "year_norm", 
+    "lang_norm", 
+    "pct_pos_total_norm", 
+    "supports_english", 
+    "for_mature_audiences"
+]
+tag_cols = [f"tag_{tag}".replace(" ", "_").replace("-", "_") for tag in top_tags]
+processed_df = raw_df[output_cols + tag_cols]
+
+processed_df.to_csv(PROCESSED_PATH, index=False)
+print(f"[INFO] Processed dataframe saved in {PROCESSED_PATH} with {processed_df.shape[0]} rows and {processed_df.shape[1]} columns")
+
+# Build exploration dataframe
+output_cols = [
+    "name", 
+    "price",
+    "required_age", 
+    "release_date", 
+    "pct_pos_total", 
+    "tags",
+    "supports_english", 
+    "lang_count"
+]
+exploration_df = raw_df[output_cols]
+
+exploration_df.to_csv(EXPLORATION_PATH, index=False)
+print(f"[INFO] Exploration dataframe saved in {EXPLORATION_PATH} with {exploration_df.shape[0]} rows and {exploration_df.shape[1]} columns")
